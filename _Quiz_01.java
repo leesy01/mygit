3d49f38827b2cad513bb68cbf3bf8bc110378c23
leesy01 <161338570+leesy01@users.noreply.github.com>
2024-05-22 16:22:18 +0900

    Add files via upload

---
+++ _Quiz_01.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String bus = "삼암 08번";
+        int time = 3;
+        double distance = 1.2;
+        System.out.println(bus+" 버스");
+        System.out.println("약 "+time+"분 후 도착");
+        System.out.println("남은 거리 "+distance+"km");
+    }
+}
